d97cfc9102b8d3c19cef3a8a547912549f05e826
heeylaley <86746166+heeylaley@users.noreply.github.com>
2022-03-05 08:15:16 +0300

    дз 1
    исправленный task_4

--- pythalg_1/task_4.py
+++ pythalg_1/task_4.py
@@ -4,22 +4,6 @@ a = int(input('Введите 1 число: '))
 b = int(input('Введите 2 число: '))
 c = int(input('Введите 3 число: '))
 
-if a > c:
-    if a > b:
-        if b > c:
-            print(b)
-        else:
-            print(c)
-    else:
-        print(a)
-elif a > b:
-    print(a)
-elif b > c:
-    print(c)
-else:
-    print(b)
-
-# короткий вариант (но тут прогон нескольких условий сразу)
 if b < a < c or c < a < b:
     print(a)
 elif a < b < c or c < b < a:
